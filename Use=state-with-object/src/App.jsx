c82b4a37c3a105cadc9d908db07e229f179727f8
MAMON-QA <mamon.green.edu.bd@gmail.com>
2024-05-04 01:17:04 +0600

    use strate with object

---
+++ Use=state-with-object/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import From from "./From/from"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <From />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
